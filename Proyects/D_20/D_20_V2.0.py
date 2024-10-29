9906c328d4cbab1a16e6266b954c7a6ede943f9e
Pebels10 <pablo.sotogdo@gmail.com>
2024-10-29 15:37:34 +0100

    JU

--- Proyects/D_20/D_20_V2.0.py
+++ Proyects/D_20/D_20_V2.0.py
@@ -1,38 +1,45 @@
-# DADO DE ROL D20 V 2.0 (ELIF + ELSE)
+# DADO DE ROL D20 V 2.0 (ELIF + ELSE)+ MIERDA DE BUCLES ME LO HE CARGADO Y NO SE COMO. 
 
 # Primera parte: Se designa un programilla para saber si quieres tirar el dado o no. Una especie de START. 
+Bucle = 'Si'
+while Bucle == 'Si':
+    print('Adelante!') 
 
-while True: #Es un bucle para que te siga preguntando lo mismo hasta que se cumpla una condición que lo rompa. 
+  #Es un bucle para que te siga preguntando lo mismo hasta que se cumpla una condición que lo rompa. 
+    afirmativo = ('Si')
+    while afirmativo == 'Si':
+      afirmativo = input('Quieres tirar el dado? (Si/No) ')
+      if afirmativo == 'Si':
+        print('Suerte!')
+      else:
+        print( 'Sigue maquinando!!')
+        break
 
-  start = input('Quieres tirar el dado? (si/no) ')
-  afirmativo = ('si')
+      # Segunda parte: Es el dado en sí. No entiendo muy bien ni el funcionamiento de import random ni el deranmom.randint.
 
-  if start == afirmativo:
-    print('Suerte!')
-    break #Esto hace que salga del bucle infinito.
+      import random
 
-  else:
-    print( 'Sigue maquinando!!')
+      advantage = int(input('Qué ventaja tienes? '))
 
-# Segunda parte: Es el dado en sí. No entiendo muy bien ni el funcionamiento de import random ni el deranmom.randint.
+      num = random.randint(1 , 20)  # Generates a random number that's either 0 or 1
 
-import random
+      if num + advantage >= 10 and num != 20 and num != 1:
+        print('Acierto!')
+        print(num , '+' , advantage , '=' , num + advantage)
+      elif num == 1:
+        print('Pifia...')
+        print(num , '+' , advantage , '=' , num + advantage )
+      elif num == 20:
+        print('Crítico!')
+        print(num , '+' , advantage , '=' , num + advantage)
+      else:
+        print('Fallo!')
+        print(num , '+' , advantage , '=' , num + advantage)
 
-advantage = int(input('Qué ventaja tienes? '))
+    bucle = input('¿Quieres volver a jugar? (Si/No): ')
+    if bucle == 'No':
+      break
 
-num = random.randint(1 , 20)  # Generates a random number that's either 0 or 1
+print('Vuelve a jugar cuando quieras!')
 
-if num + advantage >= 10 and num != 20 and num != 1:
-  print('Acierto!')
-  print(num , '+' , advantage , '=' , num + advantage)
-elif num == 1:
-  print('Pifia...')
-  print(num , '+' , advantage , '=' , num + advantage )
-elif num == 20:
-  print('Crítico!')
-  print(num , '+' , advantage , '=' , num + advantage)
-else:
-  print('Fallo!')
-  print(num , '+' , advantage , '=' , num + advantage)
 
-## quiero añadirle un ciclo para que cuando termine vuelva a empezar pero una opción al principio del todo que sea si/no/cerrar.
--- Proyects/pruba.py
+++ Proyects/pruba.py
@@ -1,96 +1,44 @@
-# SOMBRERO SELECCIONADOR: 
+# DADO DE ROL D20 V 2.0 (ELIF + ELSE)
 
-#Este es el último añadido, me he calentado y le voy a meter un bucle:
-
-Bucle = 'Si'  # Inicializamos en "Si" para entrar en el bucle la primera vez.
+# Primera parte: Se designa un programilla para saber si quieres tirar el dado o no. Una especie de START. 
+Bucle = 'Si'
 while Bucle == 'Si':
+  if Bucle != 'si':
+    break
 
-    # Voy a empezar a formulando las preguntas y las puntuaciones. 
+  #Es un bucle para que te siga preguntando lo mismo hasta que se cumpla una condición que lo rompa. 
+  afirmativo = ('si')
+  while afirmativo == 'si':
+    afirmativo = input('Quieres tirar el dado? (si/no) ')
 
-    #PREGUNTA 1
-
-    Dia = int(input(
-        "¿Qué prefieres? \n"
-        " 1: Amanecer \n"
-        " 2: Atardecer \n Respuesta: " ))
-    if Dia == 1:
-        g1, h1 = 1, 1
-        r1, s1 = 0, 0
+    if afirmativo == 'si':
+      print('Suerte!')
+      break #Esto hace que salga del bucle infinito.
 
     else:
-        r1, s1 = 1, 1
-        g1, h1 = 0, 0
+      print( 'Sigue maquinando!!')
 
-    # Estos prints('\n') simplemente son para que en la consola aparezcan espacios y nos e junten a tope el texto. 
-    print('\n')
+  # Segunda parte: Es el dado en sí. No entiendo muy bien ni el funcionamiento de import random ni el deranmom.randint.
 
-    # PREGUNTA 2
+  import random
 
-    Muerte = int(input('Cuando muera, quiero ser recordado como: \n 1: El buenazo. \n 2: El listillo. \n 3: El rancio. \n 4: ¿Quíen dijo muerte?... MUAJAJAJA. \n Respuesta: '))
-    if Muerte == 1:
-        g2 = 2
-        r2, h2, s2 = 0, 0, 0
-    elif Muerte == 2:
-        r2 = 2
-        g2, h2, s2 = 0, 0, 0 
-    elif Muerte == 3:
-        h2 = 2
-        g2,r2, s2 = 0, 0, 0
-    else:
-        s2 = 2
-        g2, r2, h2 = 0, 0, 0
+  advantage = int(input('Qué ventaja tienes? '))
 
-    print('\n')
-    # PREGUNTA 3
+  num = random.randint(1 , 20)  # Generates a random number that's either 0 or 1
 
-    Animal = int(input('¿Cuál es tu animal favorito? \n 1: El peluuuca. \n 2: El PPN (versión black del PPM). \n'
-                        ' 3: La rata gigante vaga del subsuelo. \n 4: El que más mate, que tóxicooo. \n Respuesta: '))
-    if Animal == 1: 
-        g3 = 2
-        r3, h3, s3 = 0, 0, 0
-    elif Animal == 2:
-        r3 = 2
-        g3, h3, s3 = 0, 0, 0
-    elif Animal == 3:
-        h3 = 2
-        g3, r3, s3 = 0, 0, 0
-    else:
-        s3 =2
-        g3, r3, h3 = 0, 0, 0
+  if num + advantage >= 10 and num != 20 and num != 1:
+    print('Acierto!')
+    print(num , '+' , advantage , '=' , num + advantage)
+  elif num == 1:
+    print('Pifia...')
+    print(num , '+' , advantage , '=' , num + advantage )
+  elif num == 20:
+    print('Crítico!')
+    print(num , '+' , advantage , '=' , num + advantage)
+  else:
+    print('Fallo!')
+    print(num , '+' , advantage , '=' , num + advantage)
 
-    print('\n')
+  bucle = input('¿Quieres volver a jugar? (Si/No): ')
 
-    # De esta manera puedo ir comprobando si voy haciendo bien el código al final, voy a ir escribiendo por encima de esto así que 
-    # cuando el código esté comentario no va a tener ningún sentido.
-
-    Gryffindor = g1 + g2 + g3
-    Ravenclaw = r1 + r2 + r3
-    Hufflepuff = h1 + h2 + h3
-    Slytherin = s1 + s2 + s3
-
-    if Gryffindor > Ravenclaw and Gryffindor > Hufflepuff and Gryffindor > Slytherin:
-        print('Bienvenido a Gryffindor!! 🦁')
-    elif Ravenclaw > Gryffindor and Ravenclaw > Hufflepuff and Ravenclaw > Slytherin:
-        print('Bienvenido a Ravenclaw!! 🐧')
-    elif Hufflepuff > Gryffindor and Hufflepuff > Ravenclaw and Hufflepuff > Slytherin:
-        print('Bienvenido a Hufflepuff! 🦡')
-    elif Slytherin > Gryffindor and Slytherin > Ravenclaw and Slytherin > Hufflepuff:
-        print('Bienvenido a Slytherin! 🐍')
-
-    print('\n')
-
-    print('Tu afinidad en Howarts es...')
-    print("Gryffindor = ", g1 + g2 + g3)
-    print('Ravenclaw = ', r1 + r2 + r3)
-    print('Hufflepuff = ', h1 + h2 + h3)
-    print('Slytherin = ', s1 + s2 + s3)
-
-    Bucle = input('¿Quieres volver a probar? (Si/No): ')
-
-print("Gracias por participar!!")
-
-
-# HAY PROBLEMAS, UNO QUE SE PUEDE HACER MUCHO MÁS SIMPLIFICADO, MIRAR EL CODEDEX, DOS, QUE COMO SOLO VALE UN PUNTO Y HAY POCAS PREGUNTAS 
-# ES DEMASIADO FACIL EMPATAR EN PUNTOS Y QUE NO DECLARE QUE SEAS DE NINGUNA CASA, QUE NO ROMPE EL PROGRAMA, PERO SE SALTA ESA PARTE. 
-
-#mejorar con funciones += y con la función max() para simplificar mucho el proceso. 
+print('Vuelve a jugar cuando quieras!')
